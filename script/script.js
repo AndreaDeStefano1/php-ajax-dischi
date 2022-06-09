fdc8cbb043f262fd4e01cc78b55f80bebd35ae93
Andrea.DeStefano <enricoandrea.destefano@gmail.com>
2022-06-09 18:40:00 +0200

    Milestone 2

--- script/script.js
+++ script/script.js
@@ -1,6 +1,22 @@
 var app = new Vue({
   el: '#app',
   data: {
-    
+    selectedAuthor: 'all',
+    selectedGenre: 'all',
+    discArray: [],
+    apiUrl: 'http://localhost/php-ajax-dischi/data-api/api.php'
+
+  },
+  mounted(){
+    this.getApi()
+  },
+  methods:{
+    getApi(){
+      axios.get(this.apiUrl)
+      .then(res => {
+      
+        this.discArray = res.data.response
+      })
+    }
   }
 })
