8a8c322993e52829e03fe242d2d5e4e2b7df7068
Andrea.DeStefano <enricoandrea.destefano@gmail.com>
2022-06-09 16:02:19 +0200

    Correzioni dai suggerimenti di giovanni

--- index.php
+++ index.php
@@ -1,18 +1,18 @@
 <?php
 
-include __DIR__ . '/./api.php';
+include __DIR__ . '/api.php';
  
-foreach ($encodedJson as $key => $value) {
-  if($key == 'response'){
+foreach ($encodedJson['response'] as  $disco) {
+  
   //var_dump($value);
-  foreach ($value as $disco) {
+ 
     echo "<div><img src='$disco[poster]'> <br>" ;
     echo $disco['title'] . '<br>';
     echo $disco['author'] . '<br>';
     echo $disco['genre'] . '<br>';
     echo $disco['year'] . '</div>' ;
-  }
-  }
+ 
+  
   
 }
 ?>
